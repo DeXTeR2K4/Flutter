e0d58a656c6602e6e18b1011bc7a3ece8aa563ea
surya pratap singh <surya2pratap0singh04@gmail.com>
2022-10-30 14:36:57 +0530

    day6

--- lib/pages/Task_page.dart
+++ lib/pages/Task_page.dart
@@ -1,17 +1,22 @@
-import 'package:flutter/rendering.dart';
-import 'package:meta/dart2js.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter/material.dart';
 
-class Taskpage extends StatelessWidget {
+class Taskpage extends StatefulWidget {
+  @override
+  State<Taskpage> createState() => _TaskpageState();
+}
+
+class _TaskpageState extends State<Taskpage> {
   bool? isChecked = false;
+  String name = '';
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: Column(
-         // crossAxisAlignment: CrossAxisAlignment.start,
-          
+          // crossAxisAlignment: CrossAxisAlignment.start,
+
           children: [
             Image.asset(
               "assets/images/login_image.png",
@@ -22,7 +27,7 @@ class Taskpage extends StatelessWidget {
               height: 56,
               padding: EdgeInsets.only(left: 11, top: 6, bottom: 6),
               child: Text(
-                'Welcome Back!',
+                'Welcome Back! $name',
                 textAlign: TextAlign.left,
                 style: TextStyle(
                   fontSize: 30,
@@ -36,14 +41,17 @@ class Taskpage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 15.0, right: 15.0),
               child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Email I'd",
-                    hintText: "Enter your Email I'd",
-                    fillColor: Colors.grey.shade100,
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15))),
-              ),
+                  decoration: InputDecoration(
+                      labelText: "Email I'd",
+                      hintText: "Enter your Email I'd",
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  }),
             ),
             SizedBox(
               height: 15,
@@ -51,6 +59,7 @@ class Taskpage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 15.0, right: 15.0),
               child: TextFormField(
+                obscureText: true,
                 decoration: InputDecoration(
                     labelText: "Password",
                     hintText: "Enter your password",
@@ -96,19 +105,40 @@ class Taskpage extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    hintText: 'Sign In',
-                    hintStyle: TextStyle(fontSize: 20, color: Colors.white),
-                    border: new OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(45)),
-                    fillColor: Colors.deepPurple.shade600,
-                    filled: true),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Homepage(),
+                    ));
+              },
+              child: Text(
+                'SignIn',
+                style: TextStyle(fontSize: 20),
               ),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(45)),
+                  minimumSize: Size(400, 60)),
             ),
+
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 15, right: 15),
+            //   child: ElevatedButton(
+            //       onPressed: () {},
+            //       child: TextFormField(
+            //         textAlign: TextAlign.center,
+            //         decoration: InputDecoration(
+            //           hintText: 'SignIn',
+            //           hintStyle: TextStyle(
+            //             fontSize: 20,
+            //             color: Colors.white,
+            //           ),
+
+            //           border: InputBorder(borderSide: BorderRadius.circular(radius))
+            //       )),
+            // ),
             TextButton(onPressed: () {}, child: Text('Continue by using')),
             Column(
               children: [
@@ -122,7 +152,7 @@ class Taskpage extends StatelessWidget {
             //   ],
             // ),
             SizedBox(
-              height:39,
+              height: 39,
             ),
             Padding(
               padding: const EdgeInsets.only(left: 105),
